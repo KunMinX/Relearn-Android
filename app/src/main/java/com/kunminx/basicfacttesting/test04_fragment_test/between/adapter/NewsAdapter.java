3c7dab29084821b758a680f965ba45c3dc524dd7
KunMinX <kunminx@gmail.com>
2019-07-05 10:19:19 +0800

    添加fragment adapter

---
+++ app/src/main/java/com/kunminx/basicfacttesting/test04_fragment_test/between/adapter/NewsAdapter.java
@@ -0,0 +1,74 @@
+package com.kunminx.basicfacttesting.test04_fragment_test.between.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.kunminx.basicfacttesting.R;
+import com.kunminx.basicfacttesting.test04_fragment_test.between.bean.News;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+/**
+ * Create by KunMinX at 19/7/4
+ */
+public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.NewsViewHolder> {
+
+    private OnItemClickListener mListener;
+    private List<News> mNews = new ArrayList<>();
+
+    public void refreshList(List<News> news) {
+        if (news != null) {
+            mNews.clear();
+            mNews.addAll(news);
+        }
+    }
+
+    @NonNull
+    @Override
+    public NewsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.adapter_fragment_list_item, parent, false);
+        return new NewsViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final NewsViewHolder holder, int position) {
+        holder.tvTitle.setText(mNews.get(holder.getAdapterPosition()).getTitle());
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mListener != null) {
+                    mListener.onItemClick(holder.getAdapterPosition(), mNews.get(holder.getAdapterPosition()));
+                }
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mNews.size();
+    }
+
+    static class NewsViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView tvTitle;
+
+        public NewsViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvTitle = itemView.findViewById(R.id.tv_title);
+        }
+    }
+
+    public void setListener(OnItemClickListener listener) {
+        mListener = listener;
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(int position, News news);
+    }
+}
