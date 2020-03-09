272cfb3357775f8d74919f0802e79793d3a5dba5
KunMinX <kunminx@gmail.com>
2020-03-09 21:32:33 +0800

    window move

--- app/src/main/java/com/kunminx/basicfacttesting/window_test/TestWindowActivity.java
+++ app/src/main/java/com/kunminx/basicfacttesting/window_test/TestWindowActivity.java
@@ -1,6 +1,8 @@
 package com.kunminx.basicfacttesting.window_test;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.TextView;
@@ -17,9 +19,12 @@ public class TestWindowActivity extends AppCompatActivity {
 
     private static final String TAG = "---TestWindowActivity";
 
-    private FloatWindow mFloatWindow;
-
     private MoveView mTvMove;
+    private View mView;
+
+    private int mLastX;
+    private int mLastY;
+    WindowManager.LayoutParams wml;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,20 +36,46 @@ public class TestWindowActivity extends AppCompatActivity {
 
 
     public void showFloatWindow(View view) {
-        if (mFloatWindow == null) {
-            mFloatWindow = new FloatWindow(TestWindowActivity.this, WindowManager.LayoutParams.TYPE_APPLICATION_PANEL);
-            mFloatWindow.setXY(0, 0);
-            View toolView = mFloatWindow.inflateAndSetContentView(R.layout.layout_float_window);
-        }
-        if (!mFloatWindow.isShowing()) {
-            mFloatWindow.show(getWindow().getDecorView());
-        }
+
+        mView = View.inflate(getApplicationContext(), R.layout.layout_float_window, null);
+        mView.setOnTouchListener((v, event) -> {
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    mLastX = (int) (event.getRawX());
+                    mLastY = (int) (event.getRawY());
+
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    int curX = (int) (event.getRawX());
+                    int curY = (int) (event.getRawY());
+                    int disX = curX - mLastX;
+                    int disY = curY - mLastY;
+
+                    wml.x = wml.x + disX;
+                    wml.y = wml.y + disY;
+                    getWindowManager().updateViewLayout(mView, wml);
+
+                    mLastX = curX;
+                    mLastY = curY;
+
+                    break;
+            }
+
+            return true;
+        });
+
+        wml = new WindowManager.LayoutParams();
+        wml.gravity = Gravity.START | Gravity.TOP;
+        wml.width = WindowManager.LayoutParams.WRAP_CONTENT;
+        wml.height = WindowManager.LayoutParams.WRAP_CONTENT;
+        wml.flags |= WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL;
+        wml.x = 0;
+        wml.y = 0;
+        getWindowManager().addView(mView, wml);
     }
 
     public void hideFloatWindow(View view) {
-        if (mFloatWindow != null) {
-            mFloatWindow.dismiss();
-        }
+        getWindowManager().removeView(mView);
     }
 
 
